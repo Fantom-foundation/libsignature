05a7bfaa687edfd34db2f2708f8f5704da25c76d
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-09-24 12:31:41 +1000

    draft sketch from Alex

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+extern crate rand;
+
+use libcommon_rs::peer::PeerId;
+use minisign_verify::{PublicKey, Signature};
+use rand::prelude::*;
+
+pub fn sign(&self, hash: String) -> Result<Signature> {
+    Signature::decode(hash).expect("Unable to decode the signature");
+}
+
+pub fn verify(&self, hash: String, signature: Signature, public_key: PublicKey) -> Result<Bool> {
+    public_key.verify(hash, &signature).expect("Signature didn't verify");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
