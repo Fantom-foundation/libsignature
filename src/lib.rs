d8b8feafa750f30f5d5e05093ad3aee297a2832d
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-10-18 15:30:28 +1100

    remove self parameter for keys generation methods

--- src/lib.rs
+++ src/lib.rs
@@ -25,14 +25,11 @@ pub trait Signature: Hash + Serialize + DeserializeOwned + Debug + Clone + Send 
 
     fn verify(&self, hash: Self::Hash, key: Self::PublicKey) -> Result<bool, Self::Error>;
 
-    fn generate_key_pair(&self) -> Result<(Self::PublicKey, Self::SecretKey), Self::Error>;
+    fn generate_key_pair() -> Result<(Self::PublicKey, Self::SecretKey), Self::Error>;
 
-    fn generate_public_key(
-        &self,
-        secret_key: Self::SecretKey,
-    ) -> Result<Self::PublicKey, Self::Error>;
+    fn generate_public_key(secret_key: Self::SecretKey) -> Result<Self::PublicKey, Self::Error>;
 
-    fn generate_secret_key(&self) -> Result<Self::SecretKey, Self::Error>;
+    fn generate_secret_key() -> Result<Self::SecretKey, Self::Error>;
 
     // TODO:
     // store secret keys on file and read from file
